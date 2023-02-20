51d9b5694dcc62136a7060fb059bfd57184bbac1
roscxn <roscxn@gmail.com>
2023-02-20 12:29:48 +0800

    Added new "superhero" feature
    - "Superhero" moves up 40px for every correct answer

--- script.js
+++ script.js
@@ -1,40 +1,10 @@
-//* --- Hide main page -> get into game play
-const restartGame = document.querySelector('#restart');
-restartGame.addEventListener("click", function() {
-  location.reload();
-});
-
-const buttonEasy = document.querySelector("#easy");
-buttonEasy.addEventListener("click", function(){
-  document.querySelector(".mainPage").style.display = "none";
-  document.querySelector(".easyPage").style.display = "block";
-  restartGame.style.display = "block";
-  
-});
-
-const buttonInt = document.querySelector("#int");
-buttonInt.addEventListener("click", function(){
-  document.querySelector(".mainPage").style.display = "none";
-  document.querySelector(".intPage").style.display = "block";
-  restartGame.style.display = "block";
-  countDown;
-});
-
-const buttonHard = document.querySelector("#hard");
-buttonHard.addEventListener("click", function(){
-  document.querySelector(".mainPage").style.display = "none";
-  document.querySelector(".hardPage").style.display = "block";
-  restartGame.style.display = "block";
-});
-
-
 // Question & Answer input
 let resultEasy;
 let resultInt;
 let resultHard;
 function calculate() {
   const num1 = Math.floor(Math.random() * 10);
-  const num2 = Math.floor(Math.random() * 100);
+  const num2 = Math.floor(Math.random() * 10);
   const num3 = Math.floor(Math.random() * 100);
   document.querySelector("#num1").innerText = num1;
   document.querySelector("#num2").innerText = num2;
@@ -50,22 +20,43 @@ function calculate() {
   resultHard = num1 * num2;
 }
 
-// **------- Easy level ------- 
+// Restart Button
+const restartGame = document.querySelector('#restart');
+restartGame.addEventListener("click", function() {
+  location.reload();
+});
 
-// Easy - Timer -> https://stackoverflow.com/questions/31106189/create-a-simple-10-second-countdown
+// Superhero -> Moving image ref: https://www.includehelp.com/code-snippets/move-object-with-arrow-keys-using-javascript-function.aspx#:~:text=Move%20object%20(image)%20with%20arrow,moved%20on%20the%20browser%20screen.
+
+const superHero = document.querySelector("#superhero");
+  superHero.style.position = "absolute";
+  superHero.style.left = "530px";
+  superHero.style.top = "730px";
+
+function moveUp() { 
+  superHero.style.top = parseInt(superHero.style.top) - 40 + "px";
+}
+
+
+//** ----- Easy level ----- **
+const buttonEasy = document.querySelector("#easy");
+buttonEasy.addEventListener("click", function(){
+  document.querySelector(".mainPage").style.display = "none";
+  document.querySelector(".easyPage").style.display = "block";
+  restartGame.style.display = "block";
+  // Timer -> https://stackoverflow.com/questions/31106189/create-a-simple-10-second-countdown
 let time = 20; 
 let countDown = setInterval(function(){
-    if(time === 0){
-    clearInterval(countDown);
-    const display = document.querySelector("#prompt");
-    display.innerText = "Time's up! You lose!";
-    document.querySelector("#ans").disabled = true;
+  if(time === 0){
+  clearInterval(countDown);
+  const display = document.querySelector("#prompt");
+  display.innerText = "Time's up! You lose!";
+  document.querySelector("#ans").disabled = true;
   }
   document.querySelector("#timer").innerHTML = time
   time -= 1;
 }, 1000);
 
-// Easy - Press "enter" using keyboard -> https://www.w3schools.com/howto/howto_js_trigger_button_enter.asp
 const input = document.querySelector("#ans");
 const display = document.querySelector("#prompt");
 input.addEventListener("keypress", function(event) {
@@ -74,19 +65,25 @@ input.addEventListener("keypress", function(event) {
   if (input.value == resultEasy) {
         calculate();
         display.innerText = "Correct";
+        moveUp();
       }
   else {
         calculate();
         display.innerText = "Wrong";
+
     }
     input.value = "";
   }});
   calculate();
+  moveUp();
+});
 
-
-// **------- Intermediate level ------- 
-
-// Int - Timer 
+//** ----- Int level ----- **
+const buttonInt = document.querySelector("#int");
+buttonInt.addEventListener("click", function(){
+  document.querySelector(".mainPage").style.display = "none";
+  document.querySelector(".intPage").style.display = "block";
+  restartGame.style.display = "block";
 let time2 = 20; 
 let countDown2 = setInterval(function(){
     if(time2 === 0){
@@ -98,8 +95,8 @@ let countDown2 = setInterval(function(){
   document.querySelector("#timer2").innerHTML = time2;
   time2 -= 1;
 }, 1000);
+});
 
-// Int - Enter Key
 const input2 = document.querySelector("#ans2");
 const display2 = document.querySelector("#prompt2");
 input2.addEventListener("keypress", function(event2) {
@@ -108,6 +105,7 @@ input2.addEventListener("keypress", function(event2) {
   if (input2.value == resultInt) {
         calculate();
         display2.innerText = "Correct";
+        moveUp();
       }
   else {
         calculate();
@@ -116,12 +114,15 @@ input2.addEventListener("keypress", function(event2) {
     input2.value = "";
   }});
   calculate();
+  moveUp();
 
-
-// **------- Hard level ------- 
-
-// Hard - Timer
-let time3 = 20; 
+//** ----- Hard level ----- **
+const buttonHard = document.querySelector("#hard");
+buttonHard.addEventListener("click", function(){
+  document.querySelector(".mainPage").style.display = "none";
+  document.querySelector(".hardPage").style.display = "block";
+  restartGame.style.display = "block";
+  let time3 = 20; 
 let countDown3 = setInterval(function(){
     if(time3 === 0){
     clearInterval(countDown3);
@@ -132,8 +133,8 @@ let countDown3 = setInterval(function(){
   document.querySelector("#timer3").innerHTML = time3;
   time3 -= 1;
 }, 1000);
+});
 
-// Hard - Enter Key
 const input3 = document.querySelector("#ans3");
 const display3 = document.querySelector("#prompt3");
 input3.addEventListener("keypress", function(event3) {
@@ -142,6 +143,7 @@ input3.addEventListener("keypress", function(event3) {
   if (input3.value == resultHard) {
         calculate();
         display3.innerText = "Correct";
+        moveUp();
       }
   else {
         calculate();
@@ -149,4 +151,5 @@ input3.addEventListener("keypress", function(event3) {
     }
     input3.value = "";
   }});
-  calculate();
+calculate();
+moveUp();
